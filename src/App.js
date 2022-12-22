5693f0833834035b052c786d2cbc1d3836769c12
chrisyhlee <leechr8393@gmail.com>
2022-12-22 13:50:15 -0800

    setup on off user stories

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import "./App.css"
 import LightSwitch from "./components/LightSwitch"
 
 const App = () => {
-  const [lightSwitchStatus, setLightSwitchStatus] = useState("off")
+  // const [lightSwitchStatus, setLightSwitchStatus] = useState(false)
+
+  // const turnLight = () => {
+  //   if (lightSwitchStatus === false){
+  //     setLightSwitchStatus(true)
+  //   } else {
+  //     setLightSwitchStatus(false)
+  //   }
+  // }
+
+  // let lightStatus = ""
+  // if (lightSwitchStatus === false) {
+  //   lightStatus = "Off"
+  // } else {
+  //   lightStatus = "On"
+  // }
 
   return (
     <>
       <h1>Lightbulb Challenge</h1>
+      {/* <LightSwitch lightStatus={lightStatus} turnLight={turnLight} lightSwitchStatus={lightSwitchStatus}/>
+      <LightSwitch lightStatus={lightStatus} turnLight={turnLight} lightSwitchStatus={lightSwitchStatus}/> */}
       <LightSwitch />
+      <LightSwitch />
+      <LightSwitch />
+
     </>
   )
 }
+
 
 export default App
--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,9 +1,32 @@
 import React from "react"
+import { useState } from "react"
 
 const LightSwitch = () => {
+  const [lightSwitchStatus, setLightSwitchStatus] = useState(false)
+
+  const turnLight = () => {
+    if (lightSwitchStatus === false){
+      setLightSwitchStatus(true)
+    } else {
+      setLightSwitchStatus(false)
+    }
+  }
+
+  let lightStatus = ""
+  if (lightSwitchStatus === false) {
+    lightStatus = "Off"
+  } else {
+    lightStatus = "On"
+  }
+
   return (
     <>
-      <div className="lightbulb"></div>
+      <div 
+        onClick={turnLight} 
+        className="lightbulb" 
+        style={{backgroundColor: lightSwitchStatus ? 'yellow' : 'white'}}>
+        {lightStatus}
+      </div>
     </>
   )
 }
